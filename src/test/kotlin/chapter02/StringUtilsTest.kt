4d53303d6190ec76712aa26154670f9ee7d30654
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-05-26 03:19:00 +0900

    chapter02
    - Add StringUtilsTest

---
+++ src/test/kotlin/chapter02/StringUtilsTest.kt
@@ -0,0 +1,83 @@
+package chapter02
+
+import me.bossm0n5t3r.chapter02.StringUtils
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StringUtilsTest {
+    @Test
+    fun shouldNullWhenStrIsNull() {
+        assertThat(StringUtils.substringsBetween(null, null, null))
+            .isEqualTo(null)
+    }
+
+    @Test
+    fun shouldNullWhenOpenIsNullOrEmpty() {
+        assertThat(StringUtils.substringsBetween("", null, null))
+            .isEqualTo(null)
+
+        assertThat(StringUtils.substringsBetween("", "", null))
+            .isEqualTo(null)
+    }
+
+    @Test
+    fun shouldNullWhenCloseIsNullOrEmpty() {
+        assertThat(StringUtils.substringsBetween("", "open", null))
+            .isEqualTo(null)
+
+        assertThat(StringUtils.substringsBetween("", "open", ""))
+            .isEqualTo(null)
+    }
+
+    @Test
+    fun shouldEmptyListWhenStrIsEmpty() {
+        assertThat(StringUtils.substringsBetween("", "open", "close"))
+            .isEqualTo(emptyList<String>())
+    }
+
+    @Test
+    fun shouldNullWhenStrDoesNotContainsOpen() {
+        assertThat(StringUtils.substringsBetween("str", "open", "str"))
+            .isEqualTo(null)
+    }
+
+    @Test
+    fun shouldNullWhenStrDoesNotContainsClose() {
+        assertThat(StringUtils.substringsBetween("str", "str", "close"))
+            .isEqualTo(null)
+    }
+
+    @Test
+    fun shouldNullWhenStrContainsOpenAndCloseRespectively() {
+        assertThat(StringUtils.substringsBetween("a", "a", "a"))
+            .isEqualTo(null)
+    }
+
+    @Test
+    fun shouldNonEmptyListWhenStrContainsOpenAndClose() {
+        assertThat(StringUtils.substringsBetween("axbaybazb", "a", "b"))
+            .isEqualTo(listOf("x", "y", "z"))
+        assertThat(StringUtils.substringsBetween("axbaybazb", "axb", "azb"))
+            .isEqualTo(listOf("ayb"))
+    }
+
+    @Test
+    fun shouldContainsEmptyStringWhenOpenAndCloseAreTogether() {
+        assertThat(StringUtils.substringsBetween("ababab", "a", "b"))
+            .isEqualTo(listOf("", "", ""))
+    }
+
+    @Test
+    fun whenStrContainsWhitespace() {
+        assertThat(StringUtils.substringsBetween(" ", " ", " "))
+            .isEqualTo(null)
+        assertThat(StringUtils.substringsBetween("No pain, No gain", "No", "No"))
+            .isEqualTo(listOf(" pain, "))
+    }
+
+    @Test
+    fun whenStrIsKorean() {
+        assertThat(StringUtils.substringsBetween("제발 한 번에 푹 자게 해주세요", "제발", "해주세요"))
+            .isEqualTo(listOf(" 한 번에 푹 자게 "))
+    }
+}
