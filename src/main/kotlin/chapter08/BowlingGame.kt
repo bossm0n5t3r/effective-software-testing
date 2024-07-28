1333c3f1e53a5b56e70cd4f1f39cb8574e7ff57c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-07-28 20:44:20 +0900

    chapter08

--- src/main/kotlin/chapter08/BowlingGame.kt
+++ src/main/kotlin/chapter08/BowlingGame.kt
@@ -2,10 +2,36 @@ package me.bossm0n5t3r.chapter08
 
 object BowlingGame {
     fun calculateScore(framesString: String): Int {
-        TODO("Not yet implemented")
+        val frameList = framesString.toFrameList()
+        require(frameList.size == 10) { "Frames count should be ten." }
+        return -1
     }
 
     private fun String.toFrameList(): List<Frame> {
-        TODO("Not yet implemented")
+        return this
+            .drop(1)
+            .dropLast(1)
+            .split("] [")
+            .map { "[$it]".toFrame() }
+    }
+
+    private fun String.toFrame(): Frame {
+        val split =
+            this
+                .drop(1)
+                .dropLast(1)
+                .split(" ")
+        if (split.size == 1) {
+            require(split.first() == "X") { "Invalid frame format: $this" }
+            return Frame(10)
+        }
+        val firstPins = split.first().toInt()
+        val secondPins =
+            if (split.last() == "/") {
+                10 - firstPins
+            } else {
+                split.last().toInt()
+            }
+        return Frame(firstPins, secondPins)
     }
 }
